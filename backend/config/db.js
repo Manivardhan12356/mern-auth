03e3cdd60a98fa5210e4c94ef24937b3f45f24b6
Mani vardhan <manivardhan12356@gmail.com>
2023-10-30 23:18:34 +0530

    made cahnges

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
       const con = await mongoose.connect('mongodb+srv://manivardhan12356:mani!vardhan1428@ecommercedata.11fcaed.mongodb.net/ecommercedata?retryWrites=true&w=majority', {
          useNewUrlParser: true,
          useUnifiedTopology: true,
-         // Additional mongoose opt
+
       })
       console.log(`mongo db connected ${con.connection.host}`)
    } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use('/api/users', userRouter);
 
 if (process.env.NODE_ENV == 'production') {
    const __dirname = path.resolve();
-   app.use(express.static(path.join(__dirname, 'frontend/dist')));
+   app.use(express.static(path.join(__dirname, '/frontend/dist')));
    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
 } else {
    app.get('/', (req, res) => {
