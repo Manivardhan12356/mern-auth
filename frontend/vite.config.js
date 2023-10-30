7c9fcf010b53a13ed12ae1fd075f01f1d7aa10b1
Mani vardhan <manivardhan12356@gmail.com>
2023-10-30 23:50:56 +0530

    added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: "http://localhost:8000",
+        target: "https://ecommerce-mern-t1pz.onrender.com",
         changeOrigin: true,
       }
     }
